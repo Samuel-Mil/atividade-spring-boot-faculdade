521d48206ac0dc5a23d7365523c029ef707d40c1
Samuel Milhomens Martins <samuel.mil.mar@gmail.com>
2024-10-09 14:39:09 -0300

    First name

---
+++ src/main/java/com/br/samuel/SamuelApplication.java
@@ -0,0 +1,13 @@
+package com.br.samuel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamuelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamuelApplication.class, args);
+	}
+
+}
